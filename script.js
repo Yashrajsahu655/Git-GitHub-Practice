b1fb164506a18cad9f1ce92e07085e7e0d03c25c
Yashraj sahu <yashrajsahu655@gmail.com>
2024-05-05 02:45:36 +0530

    some changes in the index.html & script.js regarding text change

--- script.js
+++ script.js
@@ -1,5 +1,12 @@
 const button = document.getElementById('colorButton');
+const Pbutton = document.getElementById('Pbutton');
+const p = document.querySelector('p');
 
 button.addEventListener('click', function () {
     button.style.backgroundColor === 'blue'?button.style.backgroundColor = 'red':button.style.backgroundColor = 'blue';
 });
+
+Pbutton.addEventListener('click', function () {
+    p.innerText = "hello"
+});
+
