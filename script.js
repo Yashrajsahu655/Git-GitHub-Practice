a318107257db28b82fe29e72cb5ca262461e34fa
Yashraj sahu <yashrajsahu655@gmail.com>
2024-05-05 01:45:47 +0530

    color in script.js (red->orange)

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ const button = document.getElementById('colorButton');
 
 button.addEventListener('click', function () {
     if (button.style.backgroundColor === 'blue') {
-        button.style.backgroundColor = 'red';
+        button.style.backgroundColor = 'orange';
     } else {
         button.style.backgroundColor = 'blue';
     }
